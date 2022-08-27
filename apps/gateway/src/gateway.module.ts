8c080dacc1364692a31b092bd943856490f08a44
Nisan Ayash <nisanayash93@gmail.com>
2022-08-27 13:45:51 +0300

    feat(gateway): config added

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,28 +1,38 @@
 import { Module } from '@nestjs/common';
-import {
-  ClientProxyFactory,
-  ClientsModule,
-  Transport,
-} from '@nestjs/microservices';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_GATEWAY_PORT: Joi.number().required(),
+        AUTH_SERVICE_PORT: Joi.number().required(),
+        AUTH_SERVICE_HOST: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [GatewayController],
   providers: [
     {
       provide: 'AUTH_SERVICE',
-      useFactory: (config) => {
+      useFactory: (config: ConfigService) => {
+        const port = config.get('AUTH_SERVICE_PORT');
+        const host = config.get('AUTH_SERVICE_HOST');
         return ClientProxyFactory.create({
           transport: Transport.TCP,
           options: {
-            port: 3001,
-            host: 'localhost',
+            port,
+            host,
           },
         });
       },
-      inject: [],
+      inject: [ConfigService],
     },
     GatewayService,
   ],
